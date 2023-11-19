7cbb00b9a46c0a76771de0958be9391b80e5dfdc
Veer <veerjhabak47@gmail.com>
2023-11-19 20:06:49 +0530

    first commit

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
-import { config } from "dotenv";
-config();
+const dotenv = require("dotenv");
+dotenv.config();
 
-import { connect } from "mongoose";
+const mongoose = require("mongoose");
 const mongoURI = `mongodb+srv://promag:${process.env.MONGODB_PASSWORD}@cluster0.tiibnnq.mongodb.net/`;
 
 const connectToMongo = () => {
-  connect(mongoURI);
+  mongoose.connect(mongoURI);
 };
 
-export default connectToMongo;
+module.exports = connectToMongo;
 
 // mongo cloud URI: 'mongodb+srv://promag:<password>@cluster0.tiibnnq.mongodb.net/'
 // const mongoURI = 'mongodb://localhost:27017/ProjectManagementTool?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
